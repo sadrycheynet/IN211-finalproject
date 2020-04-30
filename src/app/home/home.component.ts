68dac85c4aa8f561cdf1bd800c08fee54e985b7f
sadrycheynet <sadry.cheynet974@gmail.com>
2020-04-30 17:40:23 +0200

    Adding translation for home component. [WARN] This is not fixed yet: we still have to print tags without unescaping it

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IHomeLangSet } from '../constants';
+import { LangService } from '../lang.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  translation:IHomeLangSet = null;
 
-  constructor() { }
+  constructor(private lang:LangService) { }
 
   ngOnInit(): void {
+    this.lang.translation$.subscribe(translation => {
+      this.translation=translation.home
+    })
   }
 
 }
